cb7ed5c126f847c2b4586c50e4e8481c17338417
RandyFull8 <randyfull8@gmail.com>
2022-10-27 10:04:30 -0500

    ortografia

--- src/app/models/serviceModel.ts
+++ src/app/models/serviceModel.ts
@@ -1,72 +1,75 @@
 export class ServiceModel {
 
     idSolicitud :number=0
-    
+
     nombreSolicitante :String=''
-    
+
     correo :String=''
-    
+
     fechaSolicitud:String=''
-    
+
     horaSolicitud :String=''
-    
+
     area :String=''
-    
+
     maquina :String=''
-    
+
     dispositivo:String=''
-    
+
     descripcionProblema :String=''/**5 */
-    
+
     nomina :number=0
-    
+
     nombre :String=''
-    
+
     fechaInicio:String=''
-    
+
     horaInicio :String=''
-    
+
     diagnostico:String=''/**1 */
-    
+
     tipoFalla :String=''
-    
+
     emailSent :String=''
 
     nomina2:number=0
-    
+
     nombre2 :String=''
 
     asignacion:String=''
-    
+
     generoParo :String=''
-    
+
     paroCorrectivo:number=0
-    
+
     paroOperativo :number=0
-    
+
     paroRefaccion :number=0
-    
+
     tiempoTotal :String=''
-    
+
     grasaUtilizada:String=''
-    
+
     refaMateHerra :String=''/**3 */
 
     tareasEjecutadas :String=''/**2 */
-    
+
     fechaFinal :String=''
-    
+
     horaFinal :String=''
-    
+
     trabajoSanitizado:String=''
-    
+
     estatusActividad :String=''
-    
+
     firmaSolicitante :number=0
 
     nombre3:string=''
-    
+
     emailSent2 :String=''
+
     fechaFirma:String=''
+
     horaFirma:String=''
+
 }
